1e0ffd0fc6419c7a1909867d41728985737322c0
THANHLE510 <111995033+THANHLE510@users.noreply.github.com>
2023-05-31 09:58:29 +0700

    Add files via upload

---
+++ Zoo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    class Animal
+    {
+        protected int weight;
+        protected string name;
+
+        public void SetMe(int weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Name: " + name + ", Weight: " + weight);
+        }
+    }
+
+    class Lion : Animal
+    {
+        public Lion(int weight, string name)
+        {
+            base.SetMe(weight, name);
+        }
+    }
+
+    class Tiger : Animal
+    {
+        public Tiger(int weight, string name)
+        {
+            base.SetMe(weight, name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiger tiger = new Tiger(100, "tiger");
+            Lion lion = new Lion(200, "lion");
+
+            tiger.Show();
+            lion.Show();
+            
+        }
+    }
+}
